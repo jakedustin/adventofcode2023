f522172e34076ec7e158bb1d5f0dafed8c861ee3
Jake Dustin <jakedustin097@gmail.com>
2023-12-04 20:08:39 -0700

    blah blah blah

--- SolutionFactory.py
+++ SolutionFactory.py
@@ -1,4 +1,5 @@
 from day1.DayOneSolution import DayOneSolution as day_one
+from day2.DayTwoSolution import DayTwoSolution as day_two
 
 class SolutionFactory:
 
@@ -6,5 +7,7 @@ class SolutionFactory:
     def create_solver(solver_type, input_data):
         if solver_type == 1:
             return day_one(input_data)
+        elif solver_type == 2:
+            return day_two(input_data)
         else:
             raise ValueError("That solution has not been implemented yet.")
---
+++ day2/DayTwoSolution.py
@@ -0,0 +1,10 @@
+from BaseSolution import BaseSolution
+
+class DayTwoSolution(BaseSolution):  
+    cubes = {'red': 0, 'green': 0, 'blue': 0} 
+
+    def solve(self):
+        return 10
+    
+    def get_file_name(self):
+        return "day2/"
